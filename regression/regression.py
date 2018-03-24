fc9fbe5383273eaad78e04c4bc0d31d6b31f61a6
callmeliuchu <1371772034@.com>
2018-03-24 09:03:13 +0800

    py

--- regression/regression.py
+++ regression/regression.py
@@ -2,7 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-
 def loadDataSet(fileName):
 	dataMat = []
 	labelMat = []
@@ -15,7 +14,7 @@ def loadDataSet(fileName):
 			labelMat.append(tmpArr[-1])
 	return dataMat,labelMat
 
-xArr,yArr = loadDataSet('ex0.txt')
+
 
 def standRegres(xMat,yMat):
 	xTx = xMat.T*xMat
@@ -24,26 +23,46 @@ def standRegres(xMat,yMat):
 	ws = xTx.I*(xMat.T*yMat.T)
 	return ws
 
-
-def draw(xMat,yMat):
+def draw(xMat,yMat,yHat):
+	srtInd = xMat[:,1].argsort(0)
+	xSort = xMat[srtInd][:,0,:]
 	fig = plt.figure()
 	ax = fig.add_subplot(111)
-	ax.scatter(xMat[:,1].flatten().A[0],yMat.T[:,0].flatten().A[0])
+	ax.plot(xSort[:,1],yHat[srtInd].flatten().A.T)
+	ax.scatter(xMat[:,1].flatten().A[0],yMat.T[:,0].flatten().A[0],s=2,c='red')
 	plt.show()
 
+def genMat(n):
+	ret = []
+	for i in range(n):
+		tmp = []
+		for j in range(n):
+			tmp.append(10*np.random.rand()+1)
+		ret.append(tmp)
+	return ret
 
-xMat = np.mat(xArr)
-yMat = np.mat(yArr)
-ws = standRegres(xMat,yMat)
-# draw(xMat,yMat)
-# print(len(xMat[:,1]))
-# print(len(yMat.T[:,0]))
-# arr = []
-# for i in range(len(xMat[:,1])):
-# 	x = float(xMat[:,1][i])
-# 	y = float(yMat.T[:,0][i])
-# 	arr.append([x,y])
-# print(arr)
-# print(xMat[:,1].flatten().A[0])
-# print(len(yMat.T[:,0].flatten().A[0]))
-print(ws.T)
+def lwlr(testPoint,xArr,yArr,k=1.0):
+	xMat = np.mat(xArr)
+	yMat = np.mat(yArr).T
+	m,n = np.shape(xMat)
+	weights = np.mat(np.eye((m)))
+	for j in range(m):
+		diffMat = testPoint - xMat[j,:]
+		weights[j,j] = np.exp(diffMat*diffMat.T/(-2*k**2))
+	xTx = xMat.T*(weights*xMat)
+	if np.linalg.det(xTx) == 0.0:
+		print('this matrix is singular,cannot do inverse')
+		return
+	ws = xTx.I*(xMat.T*(weights*yMat))
+	return testPoint*ws
+
+def lwlrTest(testArr,xArr,yArr,k=1.0):
+	m,n = np.shape(testArr)
+	yHat = np.zeros(m)
+	for i in range(m):
+		yHat[i] = lwlr(testArr[i],xArr,yArr,k)
+	return yHat
+
+xArr,yArr = loadDataSet('ex0.txt')
+yHat = lwlrTest(xArr,xArr,yArr,0.003)
+draw(np.mat(xArr),np.mat(yArr),np.mat(yHat).T)
